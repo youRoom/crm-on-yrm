0e3234fa2725f88552fe7b422b945e5156a95793
mat_aki <matsumura.aki@gmail.com>
2010-06-09 10:32:27 +0900

    Fix invalid links

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,6 +1,6 @@
 module CustomersHelper
 
   def entry_url(entry, group)
-    "#{Customer.base_url(group)}/entries/#{entry['id']}"
+    "#{Customer.base_url(u(group))}/entries/#{entry['id']}"
   end
 end
